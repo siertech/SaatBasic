18d03fa31d0cc5008e25dcdf5b64137524bb4fa7
siertech <thomaz-guitar@hotmail.com>
2018-07-30 17:49:30 -0300

    ----

--- WebContent/dev/app/inicio/js/inicioController.js
+++ WebContent/dev/app/inicio/js/inicioController.js
@@ -1,7 +1,7 @@
 
 "use strict";
 (function(){
-	angular.module("stapiApp").controller("inicioController",function($scope, dateUtil, $timeout, stService,$compile ,config, $http, $templateCache, $controller, $mdColorUtil, $mdColors,  ChartJs, $mdColorPalette){
+	angular.module("stapiApp").controller("inicioController",function($scope){
 
 	
 	
---
+++ WebContent/dev/app/menu/js/menuRoute.js
@@ -0,0 +1,10 @@
+"use strict";
+(function(){
+angular.module("stapiApp").config(function($routeProvider,$httpProvider){
+
+
+	
+});
+
+})();
+
